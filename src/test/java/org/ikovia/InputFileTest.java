398afbae4d3eadddffb925255aaedbbc944551be
Fatih Donmez <fathdonmez@gmail.com>
2014-12-17 23:44:15 +0200

    Dummy test case removed.

--- src/test/java/org/ikovia/InputFileTest.java
+++ src/test/java/org/ikovia/InputFileTest.java
@@ -8,12 +8,6 @@ import java.io.IOException;
 public class InputFileTest {
 
     @Test
-    public void sampleTest(){
-
-        assertEquals(1,1);
-    }
-
-    @Test
     public void checkFile() throws IOException {
 
         assertNotNull(getClass().getResource("/samples.txt"));
